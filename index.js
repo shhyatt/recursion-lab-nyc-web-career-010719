e8032494a3cc7dc5f1f57bb8902b277b829d375d
shhyatt <shhyatt@gmail.com>
2019-06-22 18:18:49 -0400

    finish

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 // Code your solution here!
+function printString(myString) {
+  console.log(myString[0]);
 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(myString) {
+  if (myString === ""){
+    return ""
+  } else {
+    return reverseString(myString.substring(1)) + myString.charAt(0);
+  }
+}
+
+function palindrome(myString) {
+  let strLen = myString.length;
+  if(strLen === 0 || strLen === 1){
+    return true;
+  }
+    if (myString[0] === myString[strLen - 1]){
+      return palindrome(myString.slice(1, strLen - 1));
+    }
+    return false;
+  }
